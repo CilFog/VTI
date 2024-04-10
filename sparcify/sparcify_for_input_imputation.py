4a3df897f30a89250c5aba4fb7567c04f66d90eb
Cecilie <cfog19@student.aau.dk>
2024-04-10 09:25:59 +0200

    add bundary box

--- sparcify/sparcify_for_input_imputation.py
+++ sparcify/sparcify_for_input_imputation.py
@@ -72,30 +72,62 @@ def write_trajectories_for_area():
     if __name__ == '__main__':
         freeze_support()
 
-        brunsbuettel_to_rendsburg_bbox:Polygon = box(minx=9.114532, miny=53.880869, maxx=9.722900, maxy=54.314921)
-        brunsbuettel_to_rendsburg_path = os.path.join(INPUT_AREA_FOLDER, 'brunsbuettel_to_rendsburg')
+        brunsbuettel_to_kiel_polygon:Polygon = Polygon([(9.148469, 53.8867593),
+                                                    (9.3086607, 53.9837313),
+                                                    (9.3567259, 54.1312446),
+                                                    (9.635504, 54.2076162),
+                                                    (9.6629698, 54.2750273),
+                                                    (9.7302611, 54.2854501),
+                                                    (9.7357542, 54.3159018),
+                                                    (9.8000041, 54.3547344),
+                                                    (9.8528758, 54.3535339),
+                                                    (9.9387065, 54.3291166),
+                                                    (10.0300304, 54.3491318),
+                                                    (10.0725492, 54.3519962),
+                                                    (10.0986417, 54.3619995),
+                                                    (10.1343473, 54.3575983),
+                                                    (10.1254209, 54.308754),
+                                                    (10.1803525, 54.306751),
+                                                    (10.1899656, 54.3611993),
+                                                    (10.0965818, 54.3783996),
+                                                    (10.0519498, 54.3672002),
+                                                    (10.0045644, 54.361078),
+                                                    (9.9613126, 54.3491948),
+                                                    (9.8693021, 54.3688003),
+                                                    (9.7951444, 54.3680003),
+                                                    (9.6961212, 54.3253338),
+                                                    (9.7002411, 54.3081114),
+                                                    (9.6330797, 54.2886346),
+                                                    (9.5904394, 54.2273655),
+                                                    (9.5286413, 54.1984551),
+                                                    (9.3343206, 54.1470089),
+                                                    (9.3174677, 54.1251252),
+                                                    (9.2707758, 53.9897028),
+                                                    (9.1285469, 53.8889118),
+                                                    (9.148469, 53.8867593)])
+        brunsbuettel_to_kiel_path = os.path.join(INPUT_AREA_FOLDER, 'brunsbuettel_to_kiel')
 
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_rendsburg_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=brunsbuettel_to_rendsburg_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_rendsburg_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=brunsbuettel_to_rendsburg_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_rendsburg_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=brunsbuettel_to_rendsburg_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_rendsburg_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=brunsbuettel_to_rendsburg_bbox)
+        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_kiel_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=brunsbuettel_to_kiel_polygon)
+        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_kiel_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=brunsbuettel_to_kiel_polygon)
+        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_kiel_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=brunsbuettel_to_kiel_polygon)
+        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=brunsbuettel_to_kiel_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=brunsbuettel_to_kiel_polygon)
 
-        aalborg_harbor_to_kattegat_bbox:Polygon = box(minx=9.841940, miny=56.970433, maxx=10.416415, maxy=57.098774)
-        aalborg_harbor_to_kattegat_path = os.path.join(INPUT_AREA_FOLDER, 'aalborg_harbor_to_kattegat')
+        # aalborg_harbor_to_kattegat_bbox:Polygon = box(minx=9.841940, miny=56.970433, maxx=10.416415, maxy=57.098774)
+        # aalborg_harbor_to_kattegat_path = os.path.join(INPUT_AREA_FOLDER, 'aalborg_harbor_to_kattegat')
 
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=aalborg_harbor_to_kattegat_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=aalborg_harbor_to_kattegat_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=aalborg_harbor_to_kattegat_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=aalborg_harbor_to_kattegat_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=aalborg_harbor_to_kattegat_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=aalborg_harbor_to_kattegat_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=aalborg_harbor_to_kattegat_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=aalborg_harbor_to_kattegat_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=aalborg_harbor_to_kattegat_bbox)
 
 
-        doggersbank_to_lemvig_bbox:Polygon = box(minx=3.5, miny=54.5, maxx=8.5, maxy=56.5)
-        doggersbank_to_lemvig_path = os.path.join(INPUT_AREA_FOLDER, 'doggersbank_to_lemvig')
+        # doggersbank_to_lemvig_bbox:Polygon = box(minx=3.5, miny=54.5, maxx=8.5, maxy=56.5)
+        # doggersbank_to_lemvig_path = os.path.join(INPUT_AREA_FOLDER, 'doggersbank_to_lemvig')
 
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=doggersbank_to_lemvig_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=doggersbank_to_lemvig_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=doggersbank_to_lemvig_bbox)
-        sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=doggersbank_to_lemvig_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_realisticly_strict_trajectories, threshold=0.0, boundary_box=doggersbank_to_lemvig_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_trajectories_realisticly, threshold=0.0, boundary_box=doggersbank_to_lemvig_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_large_time_gap_with_threshold_percentage, threshold=0.5, boundary_box=doggersbank_to_lemvig_bbox)
+        # sparcify_trajectories_with_action_for_folder(str_start_date='',str_end_date='',folder_path=doggersbank_to_lemvig_path, action=sparcify_trajectories_randomly_using_threshold, threshold=0.5, boundary_box=doggersbank_to_lemvig_bbox)
 
 def write_trajectories_for_all():
 
@@ -174,3 +206,5 @@ def move_random_files_to_original_imputation(percentage=0.1):
         logging.info(f'Finished moving {num_files_to_move} files')
     except Exception as e:
         logging.error(f'Error was thrown with {repr(e)}')
+
+write_trajectories_for_area()
