e56b7d1577abc80d243186e521257ca5c97a0c38
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2024-04-11 13:44:09 +0200

    Added logging to imputation module

--- imputation_module/imputation.py
+++ imputation_module/imputation.py
@@ -3,9 +3,11 @@ import heapq
 import json
 import os
 import networkx as nx
-#from graph_construction_module.graph import create_graph
+from data.logs.logging import setup_logger
 from .functions import calculate_interpolated_timestamps, haversine_distance, adjust_edge_weights_for_draught, adjust_edge_weights_for_cog, nodes_within_radius, nodes_to_geojson, edges_to_geojson
 from .heuristics import heuristics 
+
+LOG_PATH = 'imputation_log.txt'
 
 OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'imputation_module')
 OUTPUT_FOLDER_PATH = os.path.join(OUTPUT_FOLDER, 'output')
@@ -17,6 +19,8 @@ GRAPH_INPUT_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'g
 GRAPH_INPUT_NODES = os.path.join(GRAPH_INPUT_FOLDER, 'output\\graph_all\\100000\\nodes.geojson')
 GRAPH_INPUT_EDGES = os.path.join(GRAPH_INPUT_FOLDER, 'output\\graph_all\\100000\\edges.geojson')                       
 
+logging = setup_logger(name=LOG_PATH, log_file=LOG_PATH)
+
 if not os.path.exists(OUTPUT_FOLDER_PATH):
     os.makedirs(OUTPUT_FOLDER_PATH)
 
@@ -25,123 +29,125 @@ def load_geojson(file_path):
         return json.load(f)
     
 def create_graph_from_geojson(nodes_geojson_path, edges_geojson_path):
-    G = nx.Graph()
-    
-    # Load GeoJSON files
-    nodes_geojson = load_geojson(nodes_geojson_path)
-    edges_geojson = load_geojson(edges_geojson_path)
-    
-    # Add nodes
-    for feature in nodes_geojson['features']:
-        node_id = tuple(feature['geometry']['coordinates'][::-1])  
-        G.add_node(node_id, **feature['properties'])
-    
-    # Add edges
-    for feature in edges_geojson['features']:
-        start_node = tuple(feature['geometry']['coordinates'][0][::-1])  
-        end_node = tuple(feature['geometry']['coordinates'][1][::-1])  
-        G.add_edge(start_node, end_node, **feature['properties'])
-    
-    return G
+    try: 
+        G = nx.Graph()
+        
+        # Load GeoJSON files
+        nodes_geojson = load_geojson(nodes_geojson_path)
+        edges_geojson = load_geojson(edges_geojson_path)
+        
+        # Add nodes
+        for feature in nodes_geojson['features']:
+            node_id = tuple(feature['geometry']['coordinates'][::-1])  
+            G.add_node(node_id, **feature['properties'])
+        
+        # Add edges
+        for feature in edges_geojson['features']:
+            start_node = tuple(feature['geometry']['coordinates'][0][::-1])  
+            end_node = tuple(feature['geometry']['coordinates'][1][::-1])  
+            G.add_edge(start_node, end_node, **feature['properties'])
+        
+        return G
+    except Exception as e:
+        logging.warning(f'Error occurred trying to retrieve graph: {repr(e)}')
 
 
 def impute_trajectory():
     G = create_graph_from_geojson(GRAPH_INPUT_NODES, GRAPH_INPUT_EDGES)
 
-    print("Add Trajectory to impute")
-
     trajectory_points = []
 
-    # Open the file and read trajectory to impute
-    with open(IMPUTATION_INPUT_FOLDER_PATH, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        
-        for row in reader:
-            trajectory_point = {
-                "properties": {
-                    "latitude": float(row["latitude"]),
-                    "longitude": float(row["longitude"]),
-                    "timestamp": float(row["timestamp"]),
-                    "cog": float(row["cog"]),
-                    "sog": float(row["sog"]),
-                    "draught": float(row["draught"]),
-                    "ship_type": row["ship_type"],
+    try:
+        # Open the file and read trajectory to impute
+        with open(IMPUTATION_INPUT_FOLDER_PATH, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            
+            for row in reader:
+                trajectory_point = {
+                    "properties": {
+                        "latitude": float(row["latitude"]),
+                        "longitude": float(row["longitude"]),
+                        "timestamp": float(row["timestamp"]),
+                        "cog": float(row["cog"]),
+                        "sog": float(row["sog"]),
+                        "draught": float(row["draught"]),
+                        "ship_type": row["ship_type"],
+                    }
                 }
-            }
-            trajectory_points.append(trajectory_point)
-
-    print("Impute trajectory")
+                trajectory_points.append(trajectory_point)
+    except Exception as e:
+        logging.warning(f'Error occurred trying to retrieve trajectory to impute: {repr(e)}')
     
-    imputed_paths = []  
-    
-    for i in range(len(trajectory_points) - 1):
-        start_props = trajectory_points[i]["properties"]
-        end_props = trajectory_points[i + 1]["properties"]
+    try: 
+        imputed_paths = []  
 
-        start_point = (start_props["latitude"], start_props["longitude"])
-        end_point = (end_props["latitude"], end_props["longitude"])
+        for i in range(len(trajectory_points) - 1):
+            start_props = trajectory_points[i]["properties"]
+            end_props = trajectory_points[i + 1]["properties"]
 
-        if start_point not in G:
-            G.add_node(start_point, **start_props) 
-        if end_point not in G:
-            G.add_node(end_point, **end_props)
+            start_point = (start_props["latitude"], start_props["longitude"])
+            end_point = (end_props["latitude"], end_props["longitude"])
 
-        for node in nodes_within_radius(G, start_point, 0.001):
-            if node != start_point:  
-                distance = haversine_distance(start_point[0], start_point[1], node[0], node[1])
-                G.add_edge(start_point, node, weight=distance)
-                G.add_edge(node, start_point, weight=distance)
+            if start_point not in G:
+                G.add_node(start_point, **start_props) 
+            if end_point not in G:
+                G.add_node(end_point, **end_props)
 
-        for node in nodes_within_radius(G, end_point, 0.001): 
-            if node != end_point: 
-                distance = haversine_distance(end_point[0], end_point[1], node[0], node[1])
-                G.add_edge(end_point, node, weight=distance)
-                G.add_edge(node, end_point, weight=distance)
-        
-        direct_path_exists = G.has_edge(start_point, end_point)
+            for node in nodes_within_radius(G, start_point, 0.001):
+                if node != start_point:  
+                    distance = haversine_distance(start_point[0], start_point[1], node[0], node[1])
+                    G.add_edge(start_point, node, weight=distance)
+                    G.add_edge(node, start_point, weight=distance)
 
-        if direct_path_exists:
-            print(f"Direct path exists between {i} and {i+1}. Using direct path.")
-            path = [start_point, end_point]
-            imputed_paths.append(path)
-        
-        else:
-            max_draught = start_props.get("draught", None)
-            G_apply_draught_penalty = adjust_edge_weights_for_draught(G, start_point, end_point, max_draught)
-            G_apply_cog_penalty = adjust_edge_weights_for_cog(G_apply_draught_penalty, start_point, end_point)
+            for node in nodes_within_radius(G, end_point, 0.001): 
+                if node != end_point: 
+                    distance = haversine_distance(end_point[0], end_point[1], node[0], node[1])
+                    G.add_edge(end_point, node, weight=distance)
+                    G.add_edge(node, end_point, weight=distance)
+            
+            direct_path_exists = G.has_edge(start_point, end_point)
 
-            print("Finding path")
-            try:
-                path = nx.astar_path(G_apply_cog_penalty, start_point, end_point, heuristic=heuristics, weight='weight')
-
-                start_timestamp_unix = start_props["timestamp"]  
-                end_timestamp_unix = end_props["timestamp"]  
-                
-                nodes_within_path = [(start_props["latitude"], start_props["longitude"])] + \
-                                    [(G_apply_cog_penalty.nodes[n]["latitude"], G_apply_cog_penalty.nodes[n]["longitude"]) for n in path[1:-1]] + \
-                                    [(end_props["latitude"], end_props["longitude"])]
-                
-                interpolated_timestamps = calculate_interpolated_timestamps(nodes_within_path, start_timestamp_unix, end_timestamp_unix)
-                
-                for index, node_coordinate in enumerate(nodes_within_path):
-                    node = path[index]  
-                    if node in G_apply_cog_penalty:
-                        node_props = G_apply_cog_penalty.nodes[node]
-                        node_props.update({
-                            'timestamp': interpolated_timestamps[index],  
-                            'sog': start_props["sog"], 
-                        })
-                    else:
-                        print(f"Node {node} not found in graph.")
-                
+            if direct_path_exists:
+                path = [start_point, end_point]
                 imputed_paths.append(path)
-                print(f"Path found between point {i} and {i+1}: {path}")
+            
+            else:
+                max_draught = start_props.get("draught", None)
+                G_apply_draught_penalty = adjust_edge_weights_for_draught(G, start_point, end_point, max_draught)
+                G_apply_cog_penalty = adjust_edge_weights_for_cog(G_apply_draught_penalty, start_point, end_point)
 
-            except nx.NetworkXNoPath:
-                distance = haversine_distance(start_props["latitude"], start_props["longitude"], end_props["latitude"], end_props["longitude"])
-                G_apply_cog_penalty.add_edge(start_point, end_point, weight=distance)
-                imputed_paths.append([start_point, end_point]) 
-                print(f"Path (or direct edge) handled between point {i} and {i+1}.")
+                try:
+                    path = nx.astar_path(G_apply_cog_penalty, start_point, end_point, heuristic=heuristics, weight='weight')
+
+                    start_timestamp_unix = start_props["timestamp"]  
+                    end_timestamp_unix = end_props["timestamp"]  
+                    
+                    nodes_within_path = [(start_props["latitude"], start_props["longitude"])] + \
+                                        [(G_apply_cog_penalty.nodes[n]["latitude"], G_apply_cog_penalty.nodes[n]["longitude"]) for n in path[1:-1]] + \
+                                        [(end_props["latitude"], end_props["longitude"])]
+                    
+                    interpolated_timestamps = calculate_interpolated_timestamps(nodes_within_path, start_timestamp_unix, end_timestamp_unix)
+                    
+                    for index, node_coordinate in enumerate(nodes_within_path):
+                        node = path[index]  
+                        if node in G_apply_cog_penalty:
+                            node_props = G_apply_cog_penalty.nodes[node]
+                            node_props.update({
+                                'timestamp': interpolated_timestamps[index],  
+                                'sog': start_props["sog"], 
+                            })
+                        else:
+                            print(f"Node {node} not found in graph.")
+                    
+                    imputed_paths.append(path)
+
+                except nx.NetworkXNoPath:
+                    distance = haversine_distance(start_props["latitude"], start_props["longitude"], end_props["latitude"], end_props["longitude"])
+                    G_apply_cog_penalty.add_edge(start_point, end_point, weight=distance)
+                    imputed_paths.append([start_point, end_point]) 
+                    
+    except Exception as e:
+        logging.warning(f'Error occurred trying perform imputation: {repr(e)}')
 
     unique_nodes = set()
     edges = []
