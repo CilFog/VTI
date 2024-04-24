e59404ce8664c44dfc91b7d25768628d2227fae8
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2024-04-24 22:08:06 +0200

    Update graph.py

--- graph_construction_module/graph.py
+++ graph_construction_module/graph.py
@@ -162,12 +162,12 @@ def create_edges(G, edge_radius_threshold, bearing_threshold, nodes_file_path, e
             if nearby_index != i: 
                 nearby_node, nearby_data = node_coords_list[nearby_index]
 
-                # bearing = calculate_bearing(node, nearby_node)
+                bearing = calculate_bearing(node, nearby_node)
 
-                # if bearing <= bearing_threshold:
-                distance = haversine_distance(node[0], node[1], nearby_node[0], nearby_node[1])
-                G.add_edge(node, nearby_node, weight=distance)
-                edge_count += 1
+                if bearing <= bearing_threshold:
+                    distance = haversine_distance(node[0], node[1], nearby_node[0], nearby_node[1])
+                    G.add_edge(node, nearby_node, weight=distance)
+                    edge_count += 1
 
     print(f"Total edges created: {edge_count}")
 
@@ -185,6 +185,6 @@ def create_graph(graph_trajectories, geometric_parameter, grid_size, edge_connec
 
 def create_all_graphs():
     graph_trajectories = extract_original_trajectories()
-    create_graph(graph_trajectories, 0.0001, 'grid_200', 0.00012, 45)
+    create_graph(graph_trajectories, 0.0001, 'grid_200', 0.0003, 45)
 
 create_all_graphs()
