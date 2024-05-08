4e40152533d15e357abac759b366bcc2c988773c
Cecilie <cfog19@student.aau.dk>
2024-05-08 10:19:35 +0200

    add back to input + new create test and cell data

--- sparcify/sparcify_for_input_imputation.py
+++ sparcify/sparcify_for_input_imputation.py
@@ -12,7 +12,7 @@ from multiprocessing import freeze_support
 from data.logs.logging import setup_logger
 from concurrent.futures import ProcessPoolExecutor
 from .classes import SparsifyResult, brunsbuettel_to_kiel_polygon, aalborg_harbor_to_kattegat_bbox, doggersbank_to_lemvig_bbox, skagens_harbor_bbox
-from .sparcify_methods import sparcify_realisticly_strict_trajectories, sparcify_trajectories_realisticly, sparcify_large_time_gap_with_threshold_percentage, sparcify_trajectories_randomly_using_threshold, get_trajectory_df_from_txt
+from .sparcify_methods import sparcify_realisticly_strict_trajectories, sparcify_trajectories_realisticly, sparcify_large_time_gap_with_threshold_percentage, sparcify_trajectories_randomly_using_threshold, get_trajectory_df_from_txt, check_if_trajectory_is_dense
 
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
 CELL_TXT = os.path.join(DATA_FOLDER, 'cells.txt')
@@ -115,45 +115,166 @@ def sparcify_trajectories_with_action_for_folder(
     finished_time = t.perf_counter() - initial_time
     logging.info(f'Reduced on avg. pr trajectory: {reduced_avg} for {total_trajectories} trajectories. Reduced points in total: {total_reduced_points}/{total_number_of_points}. Elapsed time: {finished_time:0.4f} seconds')   
 
-def move_random_files_to_test_and_validation(percentage=0.2):
+def move_random_files_to_test_and_validation(percentage=0.1):
     os_path_split = '/' if '/' in INPUT_GRAPH_FOLDER else '\\'
-    all_files = []
     directories_with_moved_files = set()
-
     all_files = list(Path(INPUT_GRAPH_FOLDER).rglob('*.txt')) # List all files in the directory recursively
     all_files = [str(path) for path in all_files] # Convert Path objects to strings
-    
-    # Calculate the number of files to move
-    num_files_to_move = int(len(all_files) * percentage)
-    
-    # Randomly select the files
-    files_to_move = random.sample(all_files, num_files_to_move)
 
+    num_files_to_move_to_validation = int(len(all_files) * percentage)
+    not_dense_files:list[str] = []
+    files_moved:list[str] = []
+
+    # for test
     try:
-        logging.info('Began moving files')
-        # Move the files
-        for i, file_path in enumerate(files_to_move, start=1):
-            # Move the file to input imputation folder with vessel/mmsi folder structure
-            vessel_mmsi_folder = f'{file_path.split(os_path_split)[-3]}/{file_path.split(os_path_split)[-2]}'
-
-            # move the file to test or validation folder depending on the iteration value being odd or even
-            end_dir = os.path.join(INPUT_TEST_DATA_FOLDER_ORIGINAL_FOLDER, vessel_mmsi_folder) if (i & 1) else os.path.join(INPUT_VALIDATION_DATA_ORIGINAL_FOLDER, vessel_mmsi_folder) 
-            os.makedirs(end_dir, exist_ok=True)
-            shutil.move(file_path, end_dir)
-            directories_with_moved_files.add(os.path.dirname(file_path))
-            sys.stdout.write(f"\rMoved {i}/{num_files_to_move}")
-            sys.stdout.flush()
+        cells_paths = [f'input_graph_cells{os_path_split}9_9', f'input_graph_cells{os_path_split}9_10', f'input_graph_cells{os_path_split}9_11', f'input_graph_cells{os_path_split}10_9', f'input_graph_cells{os_path_split}10_10', f'input_graph_cells{os_path_split}10_11', f'input_graph_cells{os_path_split}11_9', f'input_graph_cells{os_path_split}11_10', f'input_graph_cells{os_path_split}11_11']
+        cells = ['9_9', '9_10', '9_11', '10_9', '10_10', '10_11', '11_9', '11_10', '11_11']
         
-        # Remove empty directories
-        empty_folders_removed = 0
-        for dir_path in directories_with_moved_files:
+        # get files (vessel samples) within all cells
+        cell_files = list(Path(INPUT_GRAPH_CELLS_FOLDER).rglob('*.txt')) # List all files in the directory recursively
+        cell_files = [str(path) for path in cell_files]
+        
+        # filter all files not within the specified cells paths
+        cell_files = [path for path in cell_files if any(cell in path for cell in cells_paths)]
+        
+        # read cells in txt
+        cells_df = pd.read_csv(CELL_TXT)
+
+        # filter cells not in the cells array
+        filtered_cells_df = cells_df[cells_df['cell_id'].isin(cells)]
+        filtered_cells_df['geometry'] = filtered_cells_df.apply(lambda row: box(row['min_lon'], row['min_lat'], row['max_lon'], row['max_lat']), axis=1)
+        cells_gdf = gpd.GeoDataFrame(filtered_cells_df, geometry='geometry', crs="EPSG:4326")
+
+        # look for files (whole trajectories) in input graph that we know intersects with the cells
+        all_files_with_cells = [path for path in all_files if any(os.path.basename(cell) in path for cell in cell_files)]
+        
+        # calculate the number of files to move to test given the percentage
+        num_files_to_move_to_test = int(len(all_files_with_cells) * percentage)
+        logging.info(f'Began moving test files {num_files_to_move_to_test}')
+        
+        # select random files
+        random_files_to_move = random.sample(all_files_with_cells, num_files_to_move_to_test)
+        
+        num_files_moved_to_test = 0
+
+        while(num_files_moved_to_test < num_files_to_move_to_test):
+            for filepath in random_files_to_move:
+                # check we are not done
+                if (num_files_moved_to_test >= num_files_to_move_to_test):
+                    break
+                
+                # ensure we do not move the same file twice
+                if (filepath in files_moved or filepath in not_dense_files):
+                    continue
+
+                trajectory_cell_df = get_trajectory_df_from_txt(filepath)
+                
+                # spatial join: find which points fall within any of the cells
+                joined_gdf = gpd.sjoin(trajectory_cell_df, cells_gdf, how='left', predicate='within')
+
+                # create the boolean column based on whether a cell was matched (i.e., `index_right` is not NaN)
+                trajectory_cell_df['within_cell'] = ~joined_gdf['index_right'].isna()     
+                change_detected = trajectory_cell_df['within_cell'] != trajectory_cell_df['within_cell'].shift(1)
+                trajectory_cell_df['group'] = change_detected.cumsum()
+            
+                # find the largest group within the boundary box
+                group_sizes = trajectory_cell_df[trajectory_cell_df['within_cell']].groupby('group').size()
+                valid_groups = group_sizes[group_sizes >= 2]
+
+                if valid_groups.empty:
+                    not_dense_files.append(os.path.basename(filepath))
+                    continue
+                    
+                largest_group_id = valid_groups.idxmax()
+
+                # Filter trajectory points based on the largest group within the boundary box
+                trajectory_cell_filtered_df = trajectory_cell_df[(trajectory_cell_df['group'] == largest_group_id) & trajectory_cell_df['within_cell']]
+
+                if (not check_if_trajectory_is_dense(trajectory_cell_filtered_df)):
+                    not_dense_files.append(filepath)
+                    continue
+
+                vessel_mmsi_folder = f'{filepath.split(os_path_split)[-3]}/{filepath.split(os_path_split)[-2]}'
+                output_folder = INPUT_TEST_DATA_FOLDER_ORIGINAL_FOLDER
+                output_folder = os.path.join(output_folder, vessel_mmsi_folder)
+
+                os.makedirs(output_folder, exist_ok=True)
+
+                output_path = os.path.join(output_folder, os.path.basename(filepath))
+                trajectory_cell_filtered_df[['latitude', 'longitude', 'timestamp', 'sog', 'cog', 'draught', 'ship_type', 'navigational_status']].reset_index(drop=True).to_csv(output_path, sep=',', index=True, header=True, mode='w') 
+                
+                # remove file from input graph
+                files_moved.append(filepath)
+                num_files_moved_to_test += 1
+                os.remove(filepath) 
+                directories_with_moved_files.add(os.path.dirname(filepath))
+                sys.stdout.write(f"\rMoved {num_files_moved_to_test}/{num_files_to_move_to_test} to test")
+                sys.stdout.flush()
+
+            if (num_files_moved_to_test < num_files_to_move_to_test):
+                random_files_to_move = random.sample(all_files_with_cells, num_files_to_move_to_test)
+
+    except Exception as e:
+        logging.error('Error was thrown with', repr(e))
+    
+    logging.info(f'\nFinished moving {num_files_moved_to_test} to test')
+
+    num_files_moved_to_validation = 0
+
+    # randomly select the files
+    random_files_to_move = random.sample(all_files, num_files_to_move_to_validation)
+
+    logging.info(f'Began moving files to validation {num_files_to_move_to_validation}')
+    try:
+        while (num_files_moved_to_validation < num_files_to_move_to_validation):
+            for filepath in random_files_to_move:
+                
+                # check if we have moved the desired number of files
+                if num_files_moved_to_validation >= num_files_to_move_to_validation: 
+                    break
+
+                # check if we have already moved the file or if it is not dense
+                if filepath in files_moved or filepath in not_dense_files:
+                    continue
+
+                trajectory_df = get_trajectory_df_from_txt(filepath)
+
+                if (not check_if_trajectory_is_dense(trajectory_df)):
+                    not_dense_files.append(filepath)
+                    continue
+
+                # move the file to input imputation folder with vessel/mmsi folder structure
+                vessel_mmsi_folder = f'{filepath.split(os_path_split)[-3]}/{filepath.split(os_path_split)[-2]}'
+
+                # move the file to validation folder 
+                end_dir = os.path.join(INPUT_VALIDATION_DATA_ORIGINAL_FOLDER, vessel_mmsi_folder)
+                os.makedirs(end_dir, exist_ok=True)
+                shutil.move(filepath, end_dir)
+                directories_with_moved_files.add(os.path.dirname(filepath))
+                num_files_moved_to_validation += 1
+                sys.stdout.write(f"\rMoved {num_files_moved_to_validation}/{num_files_to_move_to_validation} to validation")
+                sys.stdout.flush()
+                files_moved.append(filepath)
+
+            if (num_files_moved_to_validation < num_files_to_move_to_validation):
+                random_files_to_move = random.sample(all_files, num_files_to_move_to_validation)
+
+        logging.info(f'\nFinished moving {num_files_moved_to_validation} to validation')
+
+    except Exception as e:
+        logging.error(f'Error was thrown with {repr(e)}')
+
+    logging.info('Began removing empty directories')
+    empty_folders_removed = 0
+    for dir_path in directories_with_moved_files:
+        try:
             if not os.listdir(dir_path):  # Check if directory is empty
                 os.rmdir(dir_path)  # Remove empty directory
                 empty_folders_removed += 1
+        except Exception as e:
+            logging.error(f'Error was thrown with {repr(e)} for files in {dir_path}')
 
-        logging.info(f'Finished moving {num_files_to_move} files\n Removed {empty_folders_removed} empty directories from input graph')
-    except Exception as e:
-        logging.error(f'Error was thrown with {repr(e)}')
+    logging.info(f'Finished moving {num_files_moved_to_validation + num_files_moved_to_test} files\n Removed {empty_folders_removed} empty directories from input graph')
 
 def find_area_input_files():
     os_path_split = '/' if '/' in INPUT_GRAPH_FOLDER else '\\'
@@ -227,9 +348,9 @@ def find_cell_input_files():
             trajectory_df = get_trajectory_df_from_txt(file_path)
             
             # Perform a spatial join to identify points from trajectory_df that intersect harbors
-            points_cells_harbors = gpd.sjoin(trajectory_df, cells_gdf, how="left", predicate="intersects", lsuffix='left', rsuffix='right')
+            points_in_cells = gpd.sjoin(trajectory_df, cells_gdf, how="left", predicate="intersects", lsuffix='left', rsuffix='right')
 
-            for (cell_id, group) in points_cells_harbors.groupby('cell_id'):
+            for (cell_id, group) in points_in_cells.groupby('cell_id'):
                 if group.empty:
                     continue
 
@@ -252,7 +373,99 @@ def find_cell_input_files():
 
     logging.info('Finished finding area input files')        
 
-find_cell_input_files()
+def move_test_and_validation_back():
+    os_path_split = '/' if '/' in INPUT_GRAPH_FOLDER else '\\'
+    directories_with_moved_files = set()
+
+    cells_df = pd.read_csv(CELL_TXT)
+    cells_df['geometry'] = cells_df.apply(lambda row: box(row['min_lon'], row['min_lat'], row['max_lon'], row['max_lat']), axis=1)
+    cells_gdf = gpd.GeoDataFrame(cells_df, geometry='geometry', crs="EPSG:4326")
+
+    validation_files = list(Path(INPUT_VALIDATION_DATA_ORIGINAL_FOLDER).rglob('*.txt')) # List all files in the directory recursively
+    test_files = list(Path(INPUT_TEST_DATA_FOLDER_ORIGINAL_FOLDER).rglob('*.txt')) # List all files in the directory recursively
+    all_files = [str(path) for path in (validation_files+test_files)] # Convert Path objects to strings
+    num_files_to_move = len(all_files)
+
+    brunsbuettel_to_kiel_gdf = gpd.GeoDataFrame([1], geometry=[brunsbuettel_to_kiel_polygon], crs="EPSG:4326").geometry.iloc[0]
+    aalborg_harbor_to_kattegat_gdf = gpd.GeoDataFrame([1], geometry=[aalborg_harbor_to_kattegat_bbox], crs="EPSG:4326").geometry.iloc[0]
+    doggersbank_to_lemvig_gdf = gpd.GeoDataFrame([1], geometry=[doggersbank_to_lemvig_bbox], crs="EPSG:4326").geometry.iloc[0] 
+    skagen_gdf = gpd.GeoDataFrame([1], geometry=[skagens_harbor_bbox], crs="EPSG:4326").geometry.iloc[0]
+
+    areas = [
+        (brunsbuettel_to_kiel_gdf, 'brunsbuettel_to_kiel'), 
+        (aalborg_harbor_to_kattegat_gdf, 'aalborg_harbor_to_kattegat'), 
+        (doggersbank_to_lemvig_gdf, 'doggersbank_to_lemvig'),
+        (skagen_gdf, 'skagen_harbor')]
+
+    for i, file_path in enumerate(all_files, start=1):
+        vessel_mmsi_folder = f'{file_path.split(os_path_split)[-3]}/{file_path.split(os_path_split)[-2]}'
+        cell_trajectory_df = get_trajectory_df_from_txt(file_path)
+        area_trajectory_df = cell_trajectory_df.copy(deep=True)
+
+        # For grid cells
+        points_cells_harbors = gpd.sjoin(cell_trajectory_df, cells_gdf, how="left", predicate="intersects", lsuffix='left', rsuffix='right')
+
+        for (cell_id, group) in points_cells_harbors.groupby('cell_id'):
+            if group.empty:
+                continue
+
+            output_folder = INPUT_GRAPH_CELLS_FOLDER
+            output_folder = os.path.join(output_folder, str(cell_id))
+            output_folder = os.path.join(output_folder, vessel_mmsi_folder)
+
+            os.makedirs(output_folder, exist_ok=True)
+
+            output_path = os.path.join(output_folder, os.path.basename(file_path))
+            group[['latitude', 'longitude', 'timestamp', 'sog', 'cog', 'draught', 'ship_type', 'navigational_status']].reset_index(drop=True).to_csv(output_path, sep=',', index=True, header=True, mode='w') 
+
+        # for input area
+        for (area, name) in areas:
+                area_trajectory_df['within_boundary_box'] = area_trajectory_df.within(area)            
+                change_detected = area_trajectory_df['within_boundary_box'] != area_trajectory_df['within_boundary_box'].shift(1)
+                area_trajectory_df['group'] = change_detected.cumsum()
+                
+                # Find the largest group within the boundary box
+                group_sizes = area_trajectory_df[area_trajectory_df['within_boundary_box']].groupby('group').size()
+                valid_groups = group_sizes[group_sizes >= 2]
+
+                if valid_groups.empty:
+                    continue
+            
+                largest_group_id = valid_groups.idxmax()
+
+                # Filter trajectory points based on the largest group within the boundary box
+                trajectory_filtered_df = area_trajectory_df[(area_trajectory_df['group'] == largest_group_id) & area_trajectory_df['within_boundary_box']]
+                output_folder = INPUT_GRAPH_AREA_FOLDER
+                output_folder = os.path.join(output_folder, name)
+                output_folder = os.path.join(output_folder, vessel_mmsi_folder)
+
+                os.makedirs(output_folder, exist_ok=True)
+
+                output_path = os.path.join(output_folder, os.path.basename(file_path))
+                trajectory_filtered_df.reset_index(drop=True).to_csv(output_path, sep=',', index=True, header=True, mode='w') 
+
+        # move the back to original folder
+        end_dir = os.path.join(INPUT_GRAPH_FOLDER, vessel_mmsi_folder)
+        os.makedirs(end_dir, exist_ok=True)
+        shutil.move(file_path, end_dir)
+        directories_with_moved_files.add(os.path.dirname(file_path))
+        sys.stdout.write(f"\rMoved {i}/{num_files_to_move}")
+        sys.stdout.flush()
+    
+    # Remove empty directories
+    empty_folders_removed = 0
+    for dir_path in directories_with_moved_files:
+        if not os.listdir(dir_path):  # Check if directory is empty
+            os.rmdir(dir_path)  # Remove empty directory
+            empty_folders_removed += 1
+
+    logging.info(f'Finished moving {num_files_to_move} files\n Removed {empty_folders_removed} empty directories from input graph')
+
+print('started moving files back')
+move_test_and_validation_back()
+
+print('began creating test and validation data for cells')
+move_random_files_to_test_and_validation()
 #write_trajectories_for_area(INPUT_VALIDATION_DATA_ORIGINAL_FOLDER, INPUT_VALIDATION_SPARSED_AREA_FOLDER)
 #write_trajectories_for_all(INPUT_VALIDATION_DATA_ORIGINAL_FOLDER, INPUT_VALIDATION_SPARSED_ALL_FOLDER)
 #write_trajectories_for_area(INPUT_TEST_DATA_FOLDER_ORIGINAL_FOLDER, INPUT_TEST_SPARSED_AREA_FOLDER)
--- utils.py
+++ utils.py
@@ -95,7 +95,6 @@ def nodes_to_geojson(G, nodes, file_path):
     with open(file_path, 'w') as f:
         json.dump(geojson, f)
 
-
 def edges_to_geojson(G, edges, file_path):
     features = []
     for start_node, end_node in edges:
@@ -123,7 +122,6 @@ def edges_to_geojson(G, edges, file_path):
     
     with open(file_path, 'w') as f:
         json.dump(geojson, f)
-
 
 def nodes_within_radius(G, point, radius):
     # Extract node positions into a NumPy array (assuming G.nodes is a list of tuples or similar)
@@ -260,8 +258,6 @@ def calculate_interpolated_timestamps(nodes_within_path, start_timestamp_unix, e
 
     return [timestamp.strftime('%d/%m/%Y %H:%M:%S') for timestamp in timestamps]
 
-
-
 """
     Functions used in the graph module
 """
@@ -310,107 +306,85 @@ def export_graph_to_geojson(G, nodes_file_path, edges_file_path):
     with open(edges_file_path, 'w') as f:
         json.dump(edges_geojson, f)
 
+def calculate_initial_compass_bearing(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """
+    Summary: Calculates the compas bearing between consecutive rows in a dataframe
 
+    Args:
+        df_curr (gpd.GeoDataFrame): dataframe containing all first positions
+        df_next (gpd.GeoDataFrame): dataframe containing all next positions
 
+    Returns:
+        gpd.GeoDataFrame: df_curr with a bearing
+    """
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def calculate_initial_compass_bearing(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-#     """
-#     Summary: Calculates the compas bearing between consecutive rows in a dataframe
-
-#     Args:
-#         df_curr (gpd.GeoDataFrame): dataframe containing all first positions
-#         df_next (gpd.GeoDataFrame): dataframe containing all next positions
-
-#     Returns:
-#         gpd.GeoDataFrame: df_curr with a bearing
-#     """
-
-#     assert len(df_curr) == len(df_next), "DataFrames must have the same length"
+    assert len(df_curr) == len(df_next), "DataFrames must have the same length"
     
-#     # Calculate bearing
-#     x = np.sin(df_curr['diff_lon']) * np.cos(df_curr['lat_rad'])     
-#     y = np.cos(df_next['lat_rad']) * np.sin(df_curr['lat_rad']) - (
-#         np.sin(df_next['lat_rad']) * np.cos(df_curr['lat_rad']) * np.cos(df_curr['diff_lon'])
-#     )
+    # Calculate bearing
+    x = np.sin(df_curr['diff_lon']) * np.cos(df_curr['lat_rad'])     
+    y = np.cos(df_next['lat_rad']) * np.sin(df_curr['lat_rad']) - (
+        np.sin(df_next['lat_rad']) * np.cos(df_curr['lat_rad']) * np.cos(df_curr['diff_lon'])
+    )
     
-#     # Bearing is now between -180 - 180
-#     bearing_rad = np.arctan2(x, y)     
+    # Bearing is now between -180 - 180
+    bearing_rad = np.arctan2(x, y)     
     
-#     # Convert to degrees and normalize such that bearing is between 0 and 360
-#     bearing_deg = (np.degrees(bearing_rad) + 360) % 360
+    # Convert to degrees and normalize such that bearing is between 0 and 360
+    bearing_deg = (np.degrees(bearing_rad) + 360) % 360
     
-#     # Handle the last row 
-#     bearing_deg.fillna(0, inplace=True)
+    # Handle the last row 
+    bearing_deg.fillna(0, inplace=True)
 
-#     return bearing_deg
+    return bearing_deg
 
-# def get_haversine_dist_df_in_meters(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-#     """
-#     Calculate the great circle distance between two points
-#     on the earth (specified in decimal degrees)
+def get_haversine_dist_df_in_meters(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
     
-#     return: distance in meters
-#     """
-#     assert len(df_curr) == len(df_next), "DataFrames must have the same length"
+    return: distance in meters
+    """
+    assert len(df_curr) == len(df_next), "DataFrames must have the same length"
     
-#     # Haversine formula
-#     a = np.sin(df_curr['diff_lat']/2)**2 + np.cos(df_curr['lat_rad']) * np.cos(df_next['lat_rad']) * np.sin(df_curr['diff_lon']/2)**2    
-#     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    # Haversine formula
+    a = np.sin(df_curr['diff_lat']/2)**2 + np.cos(df_curr['lat_rad']) * np.cos(df_next['lat_rad']) * np.sin(df_curr['diff_lon']/2)**2    
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
     
-#     # Radius of earth in kilometers is 6371
-#     distance_km = 6371 * c
-#     dist = distance_km * 1000
-#     dist.fillna(0, inplace=True)
+    # Radius of earth in kilometers is 6371
+    distance_km = 6371 * c
+    dist = distance_km * 1000
+    dist.fillna(0, inplace=True)
     
-#     return dist
+    return dist
 
-# def get_radian_and_radian_diff_columns(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> tuple[gpd.GeoDataFrame,gpd.GeoDataFrame]:
-#     # Explicitly mark df_curr and df_next as copies to avoid SettingWithCopyWarning
-#     df_curr = df_curr.copy()
-#     df_next = df_next.copy()
+def get_radian_and_radian_diff_columns(df_curr:gpd.GeoDataFrame, df_next:gpd.GeoDataFrame) -> tuple[gpd.GeoDataFrame,gpd.GeoDataFrame]:
+    # Explicitly mark df_curr and df_next as copies to avoid SettingWithCopyWarning
+    df_curr = df_curr.copy()
+    df_next = df_next.copy()
     
-#     # Convert to radians
-#     df_curr['lat_rad'] = np.radians(df_curr['latitude'])
-#     df_curr['lon_rad'] = np.radians(df_curr['longitude'])
-#     df_next['lat_rad'] = np.radians(df_next['latitude'])
-#     df_next['lon_rad'] = np.radians(df_next['longitude'])
+    # Convert to radians
+    df_curr['lat_rad'] = np.radians(df_curr['latitude'])
+    df_curr['lon_rad'] = np.radians(df_curr['longitude'])
+    df_next['lat_rad'] = np.radians(df_next['latitude'])
+    df_next['lon_rad'] = np.radians(df_next['longitude'])
     
-#     # Calculate differences
-#     df_curr['diff_lon'] = df_curr['lon_rad'] - df_next['lon_rad']
-#     df_curr['diff_lat'] = df_curr['lat_rad'] - df_next['lat_rad']
+    # Calculate differences
+    df_curr['diff_lon'] = df_curr['lon_rad'] - df_next['lon_rad']
+    df_curr['diff_lat'] = df_curr['lat_rad'] - df_next['lat_rad']
     
     
-#     return (df_curr.fillna(0).infer_objects(), df_next)
+    return (df_curr.fillna(0).infer_objects(), df_next)
 
-# # Function to calculate distance and adjust based on COG
-# def adjusted_distance(x, y):
-#     # Calculate Haversine distance
-#     haversine_dist = geodesic((x[0], x[1]), (y[0], y[1])).m
+# Function to calculate distance and adjust based on COG
+def adjusted_distance(x, y):
+    # Calculate Haversine distance
+    haversine_dist = geodesic((x[0], x[1]), (y[0], y[1])).m
     
-#     # Example COG adjustment: if COG difference is > 45 degrees, increase distance
-#     cog_diff = abs(x[2] - y[2])
-#     cog_diff = min(cog_diff, 360-cog_diff)
+    # Example COG adjustment: if COG difference is > 45 degrees, increase distance
+    cog_diff = abs(x[2] - y[2])
+    cog_diff = min(cog_diff, 360-cog_diff)
 
-#     # apply the angle penalty
-#     haversine_dist = np.sqrt(haversine_dist ** 2 + (THETA_ANGLE_PENALTY * cog_diff /180) ** 2)
+    # apply the angle penalty
+    haversine_dist = np.sqrt(haversine_dist ** 2 + (THETA_ANGLE_PENALTY * cog_diff /180) ** 2)
 
-#     return haversine_dist
+    return haversine_dist
