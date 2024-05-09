307a2561a0072f06d16ff971485ae9241eba1d55
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2024-05-09 20:12:29 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from graph_construction_module.graph import create_graphs_for_cells, process_all
 from imputation_module.imputation import impute_trajectory
 from evaluation.compare import find_all_and_compare
 
-CELLS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI\\data\\cells.txt')
+CELLS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI//data//cells.txt')
 
 def run(node_dist_threshold, edge_dist_threshold, cog_angle_threshold, graph_output_name, graph_path, original_trajectories, imputed_trajectories):
 
@@ -42,8 +42,8 @@ node_dist_threshold = 0.001
 edge_dist_threshold = 0.001
 cog_angle_threshold = 45
 graph_output_name = 'skagen'
-graph_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), f'VTI\\graph_construction_module\\output\\{graph_output_name}_{node_dist_threshold}_{edge_dist_threshold}_{cog_angle_threshold}')
-original_trajectories = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI\\data\\input_imputation\\area\\aalborg_harbor\\random_0_5')
-imputed_trajectories = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI\\imputation_module\\output')
+graph_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), f'VTI//graph_construction_module//output//{graph_output_name}_{node_dist_threshold}_{edge_dist_threshold}_{cog_angle_threshold}')
+original_trajectories = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI//data//input_imputation//area//aalborg_harbor//random_0_5')
+imputed_trajectories = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VTI//imputation_module//output')
 
 run(node_dist_threshold, edge_dist_threshold, cog_angle_threshold, graph_output_name, graph_path, original_trajectories, imputed_trajectories)
