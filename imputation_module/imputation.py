fce18012b66e38a5f0b2a031842bc783f4c18b38
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2024-05-20 11:12:05 +0200

    Update imputation.py

--- imputation_module/imputation.py
+++ imputation_module/imputation.py
@@ -231,7 +231,7 @@ def find_and_impute_paths(G, trajectory_points, file_name, node_dist_threshold, 
         else:
             try:
                 start_draught = start_props["draught"]
-                #G = adjust_edge_weights_for_draught(G, start_point, end_point, tree, node_positions, start_draught)
+                G = adjust_edge_weights_for_draught(G, start_point, end_point, tree, node_positions, start_draught)
                 path = nx.astar_path(G, start_point, end_point, heuristic=heuristics, weight='weight')
                 imputed_paths.append(path)
             except nx.NetworkXNoPath:
